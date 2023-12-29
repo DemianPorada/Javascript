76c9f589e54cca9af4d5a557aa8a5d80846355aa
DemianPorada <129092912+DemianPorada@users.noreply.github.com>
2023-12-29 13:59:49 +0200

    .

---
+++ h-w-1.js
@@ -0,0 +1,31 @@
+const age = 14;
+
+console.log(age);
+
+const name = "Demian";
+
+console.log(name);
+
+const isStudent = true;
+
+console.log(isStudent);
+
+const myString = "the best revenge is great success";
+
+console.log(myString);
+
+let myNumber = 22;
+myNumber += 10;
+console.log(myNumber);
+
+const myNull = null;
+console.log(myNull);
+
+const yourname = prompt("Яке твоє ім`я?");
+console.log(yourname);
+alert('hello ' + yourname);
+confirm("Ви дійсно хочите покинути сторінку?")
+
+alert(" наступна дія буде небезпечна");
+
+confirm("Ви впевненні що хочете продовжити?")
