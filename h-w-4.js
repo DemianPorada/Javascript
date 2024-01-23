f5be0f83a669efe4f2eeabcb22781fc1ad83eccd
DemianPorada <129092912+DemianPorada@users.noreply.github.com>
2024-01-23 18:12:43 +0200

    Create h-w-4.js

---
+++ h-w-4.js
@@ -0,0 +1,88 @@
+// 1 завдання
+
+// const textA = 'hello!';
+// const textB = '';
+
+// // Обидва поля заповнені
+
+// // Не всі поля заповнені
+
+// if (textA.length >0 && textB >0){
+//     console.log('Обидва поля заповнені')
+// }
+// else{
+//     console.log('Не всі поля заповнені')
+// }
+
+// 2 завдання 
+
+// const numberA = 7;
+// const numberB = 4;
+
+// if(numberA + numberB >10){
+//     console.log('Сума більша за 10')
+// }
+// else{
+//     console.log('Сума менша або дорівнює 10')
+// }
+
+
+
+
+// // Сума більша за 10
+
+// // Сума менша або дорівнює 10
+
+// 3 завдання 
+
+// const message = 'javascript';
+// const equalMessage = 'JavaScript';
+
+// if(message === equalMessage){
+//     console.log('Текст містить слово JavaScript')
+// }
+// else{
+//     console.log('Текст не містить слово JavaScript')
+// }
+
+
+
+
+// Текст містить слово JavaScript
+
+
+// Текст не містить слово JavaScript
+
+
+// 4
+
+// const number = 17;
+
+// if(number >10 && number <20){
+//     console.log(' Число входить в діапазон від 10 до 20')
+// }
+// else{
+//     console.log('Число не входить в діапазон від 10 до 20')
+// }
+
+
+
+// Число входить в діапазон від 10 до 20
+
+// Число не входить в діапазон від 10 до 20
+
+
+// 5
+
+
+// const name =  'demian';
+// const email = 'demian@.gmail.com';
+// const password = '63789163';
+
+// if(name.lenght > 3 && email.includes('@') && email.includes('@.') && password.lenght >6){
+//     console.log('Ви успішно авторизувавлися!')
+// }
+// else{
+//     console.log('Помилка авторизації! спробуйте ще!')
+// }
+
