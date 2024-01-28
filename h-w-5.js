854b7c1c64ba916815d12ba810ff7bf1f6636f83
DemianPorada <129092912+DemianPorada@users.noreply.github.com>
2024-01-28 20:33:54 +0200

    /

---
+++ h-w-5.js
@@ -0,0 +1,44 @@
+// let message;
+
+// const order = 'coffee';
+
+// switch (order) {
+//     case 'tea':
+     
+//     message = 'Ви обрали чай!';
+//     break;
+//     case 'coffee':
+
+//     message = 'Ви обрали каву!';
+//     break;
+//     case 'juice':
+
+//     message = 'Ви обрали сік!';
+//     break;
+
+//     default:
+//         console.log('Ви нічого не обрали!');
+// }
+
+// console.log(message);
+
+// 2
+// let day;
+
+// const today = 'monday';
+
+// switch (today) {
+//     case 'monday':
+//     case 'tuesday':
+//     case 'wednesday':
+//     case 'thusday':
+//     case 'friday':
+//         day = 'Сьогодні робочий день!';
+//         break;
+
+//         case 'saturday':
+//         case 'sunday':
+//             day = 'Сьогодні вихідний!';
+//             break;
+// }
+// console.log(day);
