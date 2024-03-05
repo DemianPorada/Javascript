22067537c22f8d7a8633f2c9b47e5f0da6127d2a
DemianPorada <129092912+DemianPorada@users.noreply.github.com>
2024-03-05 18:12:58 +0200

    .

---
+++ h-w-6.js
@@ -0,0 +1,18 @@
+// let number = 1;
+// while (number <=10) {
+//     console.log(number)
+
+//     number += 1;
+
+// }
+
+// 2
+
+// const number = 20;
+
+// for (let i = 0; i < number; i += 2) {
+//     if (i % 1 === 0) {
+//         console.log('Парне i:', i);
+//         continue;
+//     }
+// }
