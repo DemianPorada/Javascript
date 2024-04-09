ac7430d7a6f75fc579612a41f375639769f45192
DemianPorada <129092912+DemianPorada@users.noreply.github.com>
2024-04-09 19:10:41 +0300

    .

---
+++ h-w-9.js
@@ -0,0 +1,12 @@
+// 1
+const logitems = function(array){
+    for(let i = 1; i < array.length; i +=1){
+        console.log(i);
+        console.log(array[i]);
+        console.log(
+            1
+        );
+    }
+}
+
+logitems (['Mango', 'Poly', 'Ajax'])
