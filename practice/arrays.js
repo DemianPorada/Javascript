006181e01dca8f4fb4241a31f7f656af0a6c5463
DemianPorada <129092912+DemianPorada@users.noreply.github.com>
2024-11-16 17:01:10 +0200

    /

--- practice/arrays.js
+++ practice/arrays.js
@@ -3,3 +3,18 @@ console.log(cities);
 cities [1] = "Лондон";
 cities [3] = "Буковель"
 console.log(cities);
+cities [6] = "Львів";
+console.log(cities [6]);
+console.log(cities);
+
+const userNames = ["Dima", "Dina", "Demian", "Denys"];
+console.log(userNames);
+
+for(let i = 0; i < userNames.length; i += 1) {
+    console.log(i);
+    console.log(userNames[i] += '!')
+}
+
+for(let i = 0; i < cities.length; i += 1){
+    console.log(cities[1]);
+}
