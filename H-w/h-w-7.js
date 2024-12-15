564f9edf871ba0bedd691f01555dd24161372009
DemianPorada <129092912+DemianPorada@users.noreply.github.com>
2024-12-15 18:17:05 +0200

    .

---
+++ H-w/h-w-7.js
@@ -0,0 +1,57 @@
+
+// 1
+
+// const number = [2, 3, 5];
+// console.log(number);
+// number [1] = 10
+// console.log(number);
+
+// 2
+
+// const words = ["cooffe", "tea", "water"];
+// console.log(words);
+// words [3] = "juice";
+// console.log(words);
+
+// 3
+// const numbers = [12, 31, 3, 17, 60];
+// let summa = 0;
+// for(const plural of numbers){
+//     summa += plural;
+// }
+// console.log(summa);
+
+// 4
+// const numbers = [1, 2, 3, 4, 5];
+// for (let i = 0; i < numbers.length; i += 1){
+//     console.log(numbers[i]);
+// };
+
+// 5
+// const string = ["apple", "mango", "pineapple", "strawberry", "banan"];
+// const number = 5;
+// for (let i = 0; i < string.length; i += 1 ){
+//     if(string[i].length > number){
+//         console.log(string[i]);
+//     }
+// };
+
+// 6
+// const numbers = [2, 22, 34, 45, 71, 12, 23, 44, 80, 67];
+// let maxNumber = numbers[0];
+// for (const maxNum of numbers){
+//     if (maxNum > maxNumber){
+//         maxNumber = maxNum
+//     }
+// }
+// console.log(maxNumber);
+
+// 7
+// const numbers = [1, 5, 8, 9, 12, 4, 15, 27, 30, 18, 14];
+
+// let total = 0;
+// for( const number of numbers){
+//     if(number % 2 === 0){
+//         console.log(number)
+//     }
+// };
