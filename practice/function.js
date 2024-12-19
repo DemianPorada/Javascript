e80513cd65e1e000182580381ced27c720fa3210
DemianPorada <129092912+DemianPorada@users.noreply.github.com>
2024-12-19 19:42:17 +0200

    /

--- practice/function.js
+++ practice/function.js
@@ -16,15 +16,30 @@ getSum (3, 4)
 
 
 
-const numbers = [51, 18, 13, 24, 7, 85, 19];
+// const numbers = [51, 18, 13, 24, 7, 85, 19];
 
-function findSmallesNumber(numbers){
-    let smallestNumber = numbers[0]
-    for (let i = 0; i < numbers.length; i+=1) {
-        if(numbers[i] < smallestNumber ){
-            smallestNumber = numbers[i]
-        }
-    }
+// function findSmallesNumber(numbers){
+//     let smallestNumber = numbers[0]
+//     for (let i = 0; i < numbers.length; i+=1) {
+//         if(numbers[i] < smallestNumber ){
+//             smallestNumber = numbers[i]
+//         }
+//     }
+// }
+
+// findSmallesNumber(numbers)()
+
+console.log(sum(2, 3));
+
+function sum (a, b){
+    return a+b;
 }
 
-findSmallesNumber(numbers)()
+const sum1 = function (a, b){
+    return a+b
+}
+console.log(sum1(2, 3));
+
+const repeatLog = function(n){
+    for(let i = 0; i < n; i +=1)
+}
