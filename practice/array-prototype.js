52208d0aa234ba44a0c1beb7b97f0ec294b0f26e
DemianPorada <129092912+DemianPorada@users.noreply.github.com>
2025-01-28 20:30:22 +0200

    /

---
+++ practice/array-prototype.js
@@ -0,0 +1,19 @@
+/*
+ * Array.prototype.forEach(callback(currentValue, index, array), thisArg)
+ * - Поелементо перебирає оригінальний масив
+ * - Нічого не повертає
+ * - Замінює класичний for, якщо не потрібно преривати цикл
+ */
+const numbers = [5, 10, 15, 20, 25];
+// numbers.forEach(function(number){ })
+numbers.forEach(function(number){ })
+forEach(numbers){
+    for(const number of numbers){
+        callback(5)
+    }
+    for(const number of numbers){
+        callback(5)
+    }
+}
+
+console.log(numbers)
