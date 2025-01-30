abe16f9a460c151f9047138ccf19bd302327c88a
DemianPorada <129092912+DemianPorada@users.noreply.github.com>
2025-01-30 20:32:09 +0200

    /

--- practice/array-prototype.js
+++ practice/array-prototype.js
@@ -4,16 +4,31 @@
  * - Нічого не повертає
  * - Замінює класичний for, якщо не потрібно преривати цикл
  */
-const numbers = [5, 10, 15, 20, 25];
+// const numbers = [5, 10, 15, 20, 25];
+// // numbers.forEach(function(number){ })
 // numbers.forEach(function(number){ })
-numbers.forEach(function(number){ })
-forEach(numbers){
-    for(const number of numbers){
-        callback(5)
-    }
-    for(const number of numbers){
-        callback(5)
-    }
-}
+// forEach(numbers){
+//     for(const number of numbers){
+//         callback(5)
+//     }
+//     for(const number of numbers){
+//         callback(5)
+//     }
+// }
 
-console.log(numbers)
+// console.log(numbers)
+
+
+
+// const players = [
+//     { id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: false },
+//     { id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: true },
+//     { id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true },
+//     { id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
+//     { id: 'player-5', name: 'Chelsy', timePlayed: 80, points: 48, online: true },
+//   ];
+//   const playersNames = players.map(function({name}){
+//     return name
+//   }
+// )
+// console.log(playersNames)
