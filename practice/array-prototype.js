ca2a7370c72c1de938832c7877ba9da31c9b8b5e
DemianPorada <129092912+DemianPorada@users.noreply.github.com>
2025-02-06 20:34:51 +0200

    /

--- practice/array-prototype.js
+++ practice/array-prototype.js
@@ -27,8 +27,64 @@ const players = [
     { id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
     { id: 'player-5', name: 'Chelsy', timePlayed: 80, points: 48, online: true },
   ];
-  const playersNames = players.map(function({name}){
-    return name
-  }
-)
-console.log(playersNames)
+//   const playersNames = players.map(function({name}){
+//     return name
+//   }
+// )
+// console.log(playersNames)
+
+
+
+
+
+/*
+   * Array.prototype.every()
+   * - Поелементо перебирає оригінальний масив
+   * - Повертає true якщо всі елементи масива задовільняють умову
+   */
+/*
+   * Array.prototype.some()
+   * - Поелементо перебирає оригінальний масив
+   * - Повертає true якщо хоча б один елемент масива задовільняє умову
+   */
+  
+ const anyHardcorePlayers = players.every(function(hardcore){
+  return players.online
+})
+console.log(anyHardcorePlayers)
+
+ const isAnyOnline = players.some(function(player){
+  return players.online
+ })
+ console.log(isAnyOnline)
+
+ /*
+ * Array.prototype.reduce()
+ * - Поелементо перебирає оригінальний масив
+ * - Повертає що завгодно 🤯
+ * - Замінює все на світі, але використовувати потрібно з розумом
+ */
+
+ const numbers = [5, 10, 15, 20, 25];
+
+const total = numbers.reduce(function (acc, number) {
+  console.log('number: ', number);
+  console.log('acc: ', acc);
+  return acc += number
+},0);
+console.log(total);
+
+
+const salary = {
+  mango: 100,
+  poly: 50,
+  ajax: 150,
+};
+
+const tweets = [
+  { id: '000', likes: 5, tags: ['js', 'nodejs'] },
+  { id: '001', likes: 2, tags: ['html', 'css'] },
+  { id: '002', likes: 17, tags: ['html', 'js', 'nodejs'] },
+  { id: '003', likes: 8, tags: ['css', 'react'] },
+  { id: '004', likes: 0, tags: ['js', 'nodejs', 'react'] },
+];
