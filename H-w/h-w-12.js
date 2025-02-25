ea29c5fab17cea360db6824494a64d80d85c3782
DemianPorada <129092912+DemianPorada@users.noreply.github.com>
2025-02-25 18:00:05 +0200

    /

---
+++ H-w/h-w-12.js
@@ -0,0 +1,157 @@
+//1.
+const user = {
+    mood: "tired",
+    hobby: "reading",
+    premium: true,
+  };
+  user.mood = "happy";
+  user.hobby = "skydiving";
+  user.premium = false;
+  
+  const userKeys = Object.keys(user);
+  console.log(userKeys);
+  
+  for (let key of userKeys) {
+    console.log(key);
+  }
+  
+  //2.
+  function countProps(obj) {
+    const keys = Object.keys(obj);
+    return keys.length;
+  }
+  const member = {
+    name: "anastasia",
+    age: 15,
+    hobby: "reading",
+  };
+  
+  console.log(countProps(user));
+  
+  //3.
+  const emplosees = [
+    {
+      lilit: 10,
+      salary: 17000,
+    },
+    {
+      adeline: 20,
+      salary: 15000,
+    },
+    {
+      emma: 30,
+      salary: 12000,
+    },
+    {
+      josef: 40,
+      salary: 14500,
+    },
+    {
+      anastasia: 50,
+      salary: 10000,
+    },
+    {
+      lili: 60,
+      salary: 22000,
+    },
+  ];
+  // function findBestEmployee(employees) {
+  //   let totalSalary = 0;
+  //   for (const employee of Object.key(employees)) {
+  //     totalSalary += employee.salary;
+  //   }
+  //   return totalSalary;
+  // }
+  // console.log(findBestEmployee(emplosees));
+  
+  //4.
+  const emploees = [
+    {
+      leo: 15000,
+    },
+    {
+      emma: 12000,
+    },
+    {
+      anastasia: 14500,
+    },
+    {
+      meddy: 10000,
+    },
+    {
+      lily: 22000,
+    },
+  ];
+  function countTotalSalary(emploees) {
+    let total = 0;
+    for (let employee of emploees) {
+      total += employee.salary;
+    }
+    return total;
+  }
+  console.log(countTotalSalary(emploees));
+  
+  //5.
+  function getAllPropValues(arr, prop) {
+    const values = [];
+    for (let employee of arr) {
+      if (Object.keys(employee).includes(prop)) {
+        values.push(employee[prop]);
+      }
+    }
+    return values;
+  }
+  
+  const employees = [
+    {
+      man: "worked",
+    },
+    {
+      man1: "notworked",
+    },
+    {
+      man2: "worked",
+    },
+    {
+      man3: "notworked",
+    },
+    {
+      man4: "worked",
+    },
+  ];
+  
+  console.log(getAllPropValues(employees, "man"));
+  console.log(getAllPropValues(employees, "man1"));
+  console.log(getAllPropValues(employees, "man2"));
+  
+  //6.
+  const basket = [
+    {
+      sport: "volleyball",
+      price: 100,
+      amount: 3,
+    },
+    {
+      sport: "volleyball",
+      price: 60,
+      amount: 4,
+    },
+    {
+      sport: "football",
+      price: 140,
+      amount: 5,
+    },
+  ];
+  function calculateTotalPrice(productsArray, searchName) {
+    let totalCost = 0;
+    for (let product of productsArray) {
+      if (product.sport === searchName) {
+        totalCost += product.price * product.amount;
+      }
+    }
+    return totalCost;
+  }
+  console.log(calculateTotalPrice(productsArray, "volleyball", 2));
+  console.log(calculateTotalPrice(searchName, "football", 4));
+  // Напиши функцію calculateTotalPrice(allProdcuts, productName), яка отримує масив об'єктів та ім'я продукту (значення властивості name). Повертає загальну вартість продукту (ціна * кількість).
+  // Викличи функції для перевірки працездатності твоєї реалізації.
---
+++ H-w/h-w-13.js
@@ -0,0 +1,119 @@
+// //1.
+// const user = {
+//     mood: "tired",
+//     hobby: "reading",
+//     premium: true,
+//   };
+  
+//   const { mood, hobby, premium } = user;
+//   console.log(mood, hobby, premium);
+  
+//   const userMood = user.mood;
+//   const userHobby = user.hobby;
+//   const userPremium = user.premium;
+//   console.log(userMood, userHobby, userPremium);
+//   //--------------------------------------------------
+  
+//   //2.
+//   const member = {
+//     name: "anastasia",
+//     age: 15,
+//     hobby: "reading",
+//   };
+  
+//   const { name, age, hobby } = member;
+//   console.log(name, age, hobby);
+  
+//   const memberName = member.name;
+//   const memberAge = member.age;
+//   const memberHobby = member.hobby;
+//   console.log(memberName, memberAge, memberHobby);
+//   console.log(memberName);
+//   console.log(memberAge);
+//   console.log(memberHobby);
+//   //--------------------------------------------------
+  
+//   //3.
+//   const weather = {
+//     temperature: -22,
+//     interest: 80,
+//     courseWind: 10,
+//   };
+  
+//   const { temperature, interest, courseWind } = weather;
+//   console.log(temperature, interest, courseWind);
+  
+//   const weatherTemperature = weather.temperature;
+//   const weatherInterest = weather.interest;
+//   const weatherCourseWind = weather.courseWind;
+//   console.log(weatherTemperature);
+//   console.log(weatherInterest);
+//   console.log(weatherCourseWind);
+//   //--------------------------------------------------
+  
+//   //4.
+//   const user = {
+//     name: "anastasia rubakha",
+//     email: "kjtmetntiksd@gmail.com",
+//     password: "kfdskfjsl",
+//   };
+  
+//   const { name, email, password } = user;
+//   console.log(name, email, password);
+  
+//   const userName = user.name;
+//   const userEmail = user.email;
+//   const userPassword = user.password;
+//   console.log(userName, userEmail, userPassword);
+//   //--------------------------------------------------
+  
+//   //5.
+//   const movie = {
+//     title: "your heart will be broken",
+//     director: "Anna Jane",
+//     year: 2020,
+//     rating: 9.1,
+//   };
+  
+//   const { title, director, year, rating } = movie;
+//   console.log(title, director, year, rating);
+  
+//   const movieTitle = movie.title;
+//   const movieDirector = movie.director;
+//   const movieYear = movie.year;
+//   const movieRating = movie.rating;
+//   console.log(movieTitle, movieDirector, movieYear, movieRating);
+//   //--------------------------------------------------
+  
+//   //6.
+//   const account = {
+//     balance: 1000,
+  
+//     getBalance() {
+//       return `поточний баланс ${this.balance};`;
+//     },
+  
+//     deposit(amount) {
+//       if (amount <= 0) {
+//         return "поповнення має бути більшою за 0";
+//       }
+//       this.balance += amount;
+//       return `поповнено на ${amount} ${this.getBalance()}`;
+//     },
+  
+//     withdraw(amount) {
+//       if (amount <= 0) {
+//         return "сума має бути більшою за 0";
+//       }
+//       if (amount > this.balance) {
+//         return "недостатньо коштів на рахунку";
+//       }
+//       this.balance -= amount;
+//       return `успішно знято ${amount} ${this.getBalance()}`;
+//     },
+//   };
+  
+//   console.log(account.getBalance());
+//   console.log(account.deposit(500));
+//   console.log(account.withdraw(300));
+//   console.log(account.getBalance());
