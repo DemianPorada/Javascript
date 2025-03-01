32484b1b45d58628545f1f86f299e4d4fc5a04a1
DemianPorada <129092912+DemianPorada@users.noreply.github.com>
2025-03-01 20:25:39 +0200

    /

--- H-w/h-w-14.js
+++ H-w/h-w-14.js
@@ -1,76 +1,222 @@
-const familysChildrens = [
-    {
-      name: "mama",
-      eyeColor: "blue",
-      gender: "female",
-      isActive: true,
-      email: "mama@gmail.com",
-      age: "48",
-    },
-    {
-      name: "papa",
-      eyeColor: "blue",
-      gender: "male",
-      isActive: false,
-      email: "papa@gmail.com",
-      age: "54",
-    },
-    {
-      name: "sister",
-      eyeColor: "green",
-      gender: "female",
-      isActive: false,
-      email: "sister@gmail.com",
-      age: "13",
-    },
-    {
-      name: "brother",
-      eyeColor: "brown",
-      gender: "male",
-      isActive: true,
-      email: "brother@gmail.com",
-      age: "19",
-    },
-    {
-      name: "grandmother",
-      eyeColor: "green",
-      gender: "female",
-      isActive: false,
-      email: "grandmother@gmail.com",
-      age: "79",
-    },
-    {
-      name: "grandfather",
-      eyeColor: "brown",
-      gender: "male",
-      isActive: true,
-      email: "grandfather@gmail.com",
-      age: "84",
-    },
-  ];
-  
-  //1
-  const wholeNames = familysChildrens.map((member) => member.name);
-  console.log(wholeNames);
-  //2
-  const eyeColor = familysChildrens.filter(
-    (member) => member.eyeColor === "blue"
-  );
-  console.log(eyeColor);
-  //3
-  const gender = familysChildrens.filter((member) => member.gender === "male");
-  console.log(gender);
-  //4
-  const active = familysChildrens.filter((member) => member.isActive === true);
-  console.log(active);
-  //5
-  const emailFind = "sister@gmail.com";
-  const byEmail = familysChildrens.find(
-    (member) => member.email === emailFind
-  );
-  console.log(byEmail);
-  //6
-  const ageCheck = familysChildrens.filter(
-    (member) => member.age >= 10 && member.age <= 19
-  );
-  console.log(ageCheck);
+// Робота з масивом об’єктів
+const users = [
+
+  {
+
+    id: '701b29c3-b35d-4cf1-a5f6-8b12b29a5081',
+
+    name: 'Moore Hensley',
+
+    email: 'moorehensley@indexia.com',
+
+    eyeColor: 'blue',
+
+    friends: ['Sharron Pace'],
+
+    isActive: false,
+
+    balance: 2811,
+
+    skills: ['ipsum', 'lorem'],
+
+    gender: 'male',
+
+    age: 37,
+
+  },
+
+  {
+
+    id: '7a3cbd18-57a1-4534-8e12-1caad921bda1',
+
+    name: 'Sharlene Bush',
+
+    email: 'sharlenebush@tubesys.com',
+
+    eyeColor: 'blue',
+
+    friends: ['Briana Decker', 'Sharron Pace'],
+
+    isActive: true,
+
+    balance: 3821,
+
+    skills: ['tempor', 'mollit', 'commodo', 'veniam', 'laborum'],
+
+    gender: 'female',
+
+    age: 34,
+
+  },
+
+  {
+
+    id: '88beb2f3-e4c2-49f3-a0a0-ecf957a95af3',
+
+    name: 'Ross Vazquez',
+
+    email: 'rossvazquez@xinware.com',
+
+    eyeColor: 'green',
+
+    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+
+    isActive: false,
+
+    balance: 3793,
+
+    skills: ['nulla', 'anim', 'proident', 'ipsum', 'elit'],
+
+    gender: 'male',
+
+    age: 24,
+
+  },
+
+  {
+
+    id: '249b6175-5c30-44c6-b154-f120923736f5',
+
+    name: 'Elma Head',
+
+    email: 'elmahead@omatom.com',
+
+    eyeColor: 'green',
+
+    friends: ['Goldie Gentry', 'Aisha Tran'],
+
+    isActive: true,
+
+    balance: 2278,
+
+    skills: ['adipisicing', 'irure', 'velit'],
+
+    gender: 'female',
+
+    age: 21,
+
+  },
+
+  {
+
+    id: '334f8cb3-eb04-45e6-abf4-4935dd439b70',
+
+    name: 'Carey Barr',
+
+    email: 'careybarr@nurali.com',
+
+    eyeColor: 'blue',
+
+    friends: ['Jordan Sampson', 'Eddie Strong'],
+
+    isActive: true,
+
+    balance: 3951,
+
+    skills: ['ex', 'culpa', 'nostrud'],
+
+    gender: 'male',
+
+    age: 27,
+
+  },
+
+  {
+
+    id: '150b00fb-dd82-427d-9faf-2879ea87c695',
+
+    name: 'Blackburn Dotson',
+
+    email: 'blackburndotson@furnigeer.com',
+
+    eyeColor: 'brown',
+
+    friends: ['Jacklyn Lucas', 'Linda Chapman'],
+
+    isActive: false,
+
+    balance: 1498,
+
+    skills: ['non', 'amet', 'ipsum'],
+
+    gender: 'male',
+
+    age: 38,
+
+  },
+
+  {
+
+    id: 'e1bf46ab-7168-491e-925e-f01e21394812',
+
+    name: 'Sheree Anthony',
+
+    email: 'shereeanthony@kog.com',
+
+    eyeColor: 'brown',
+
+    friends: ['Goldie Gentry', 'Briana Decker'],
+
+    isActive: true,
+
+    balance: 2764,
+
+    skills: ['lorem', 'veniam', 'culpa'],
+
+    gender: 'female',
+
+    age: 39,
+
+  },
+
+];
+
+// 1
+// Отримати масив імен всіх користувачів (поле name).
+
+const getUserNames = users => {
+  return users.map(user => user.name)
+};
+
+console.log(getUserNames(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
+
+// 2
+// Отримати масив об'єктів користувачів за кольором очей (поле eyeColor).
+
+const getUsersWithEyeColor = (users, color) => {
+  return users.filter(user => user.eyeColor === color)
+};
+
+console.log(getUsersWithEyeColor(users, 'blue'));
+ // [об'єкт Moore Hensley, об'єкт Sharlene Bush, об'єкт Carey Barr]
+
+//  3
+// Отримати масив імен користувачів за статтю (поле gender)
+
+const getUsersWithGender = (users, gender) => {
+  return users.filter(user => user.gender === gender).map(user => user.name)
+};
+
+console.log(getUsersWithGender(users, 'male')); 
+// [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
+
+// 4
+// Отримати масив тільки неактивних користувачів (поле isActive).
+
+const getInactiveUsers = users => {
+  return users.filter(user => ! user.isActive)
+};
+
+console.log(getInactiveUsers(users)); 
+// [об'єкт Moore Hensley, об'єкт Ross Vazquez, об'єкт Blackburn Dotson]
+
+// 5
+// Отримати користувача (не масив) по email (поле email, він унікальний)
+
+const getUserWithEmail = (users, email) => {
+  return users.find(user => user.email === email)
+};
+
+console.log(getUserWithEmail(users, 'shereeanthony@kog.com')); // {об'єкт користувача Sheree Anthony}
+console.log(getUserWithEmail(users, 'elmahead@omatom.com')); // {об'єкт користувача Elma Head}
