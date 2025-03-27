1e55bd646028bdf3b3b22d1090018dfba928c4b3
DemianPorada <129092912+DemianPorada@users.noreply.github.com>
2025-03-27 20:40:06 +0200

    /

--- practice/Dom.js
+++ practice/Dom.js
@@ -74,3 +74,21 @@
 
 
 // articleEl.innerHTML = articles.join(' ')
+
+
+
+const refs = {
+    output: document.querySelector('.js-output'),
+    clearBtn: document.querySelector('.js-clear'),
+  };
+  
+//  window.addEventListener('keydown', onKeypress)
+
+ function onKeypress(event){
+  // console.log('event.code: ', event.code);
+  // console.log('event.key: ', event.key);
+
+ }
+
+const closeButton = document.querySelector('[data-action="close-modal"]');
+const openButton = document.querySelector('[data-action="open-modal"]');
